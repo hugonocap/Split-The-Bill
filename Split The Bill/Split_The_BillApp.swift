66016edcaa2cd7800185ba4c0580bd5908cb3f8c
Hugo Montana <benzlbenny2k@gmail.com>
2022-11-12 20:32:54 +0200

    Initial Commit

---
+++ Split The Bill/Split_The_BillApp.swift
@@ -0,0 +1,17 @@
+//
+//  Split_The_BillApp.swift
+//  Split The Bill
+//
+//  Created by Hugo on 12.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Split_The_BillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
